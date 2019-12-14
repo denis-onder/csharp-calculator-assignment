b811378feb75fdc20b4cd9fe806e09513b2ba6e5
Denis Onder <denis_onder@protonmail.com>
2019-12-15 00:54:25 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace csharp_calculator
+{
+  class Calculator
+  {
+    public static string WriteText(string msg)
+    {
+      return msg;
+    }
+    public static string WriteNumber(int x)
+    {
+      return x.ToString();
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s = Calculator.WriteText("Hello world!");
+      Console.WriteLine(s);
+      string n = Calculator.WriteNumber(53 + 28);
+      Console.WriteLine(n);
+      Console.Read();
+    }
+  }
+}
