9c6410c2d301eb2ea04b4aa94ffcde15a1ddd3a5
Denis Onder <denis_onder@protonmail.com>
2019-12-15 01:03:10 +0100

    Assignment 2

--- Program.cs
+++ Program.cs
@@ -4,22 +4,30 @@ namespace csharp_calculator
 {
   class Calculator
   {
-    public static string WriteText(string msg)
+    private int operand1;
+    private int operand2;
+    public Calculator(int operand1, int operand2)
+    {
+      this.operand1 = operand1;
+      this.operand2 = operand2;
+    }
+    public string WriteText(string msg)
     {
       return msg;
     }
-    public static string WriteNumber(int x)
+    public string WriteNumber()
     {
-      return x.ToString();
+      return $"{this.operand1 + this.operand2}";
     }
   }
   class Program
   {
     static void Main(string[] args)
     {
-      string s = Calculator.WriteText("Hello world!");
+      Calculator c = new Calculator(53, 28); // Vrijednosti za racunanje se sada unose u konstruktor
+      string s = c.WriteText("Hello world!");
       Console.WriteLine(s);
-      string n = Calculator.WriteNumber(53 + 28);
+      string n = c.WriteNumber();
       Console.WriteLine(n);
       Console.Read();
     }
